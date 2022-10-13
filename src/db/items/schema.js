33dcc0cf60744c9ec451dbe77b974c9585b54519
Izzatillo <izzatillo.mukhtorov@ydistri.com>
2022-10-13 16:39:48 +0500

    change owner property in comment schema

--- src/db/items/schema.js
+++ src/db/items/schema.js
@@ -9,7 +9,7 @@ const itemSchema = new Schema(
     image: { type: String },
     comments: [
       {
-        owner: { type: Schema.Types.ObjectId, ref: "User" },
+        owner: [{ type: Schema.Types.ObjectId, ref: "User" }],
         text: { type: String },
       },
     ],
