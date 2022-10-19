d4d12a89aa7c12cd411b0d22556fd1cd5b65a8a0
Izzatillo <izzatillo.mukhtorov@ydistri.com>
2022-10-20 03:03:40 +0500

    fix custom input field schema

--- src/db/collections/schema.js
+++ src/db/collections/schema.js
@@ -7,13 +7,15 @@ const collectionSchema = new Schema(
     description: { type: String },
     topic: { type: String },
     image: { type: String },
-    customFields: {
-      fieldNumber: { type: Number },
-      fieldMultilineText: { type: String },
-      fieldType: { type: String },
-      fieldChecked: { type: Boolean },
-      fieldDate: { type: Date },
-    },
+    customFields: [
+      {
+        fieldNumber: { type: Number },
+        fieldMultilineText: { type: String },
+        fieldType: { type: String },
+        fieldChecked: { type: Boolean },
+        fieldDate: { type: Date },
+      },
+    ],
     owner: { type: Schema.Types.ObjectId, ref: "User" },
     items: [{ type: Schema.Types.ObjectId, require: true, ref: "Item" }],
   },
