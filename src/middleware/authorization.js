2ecdb43cd30a167865fbb2658a7bddf0bb71f33e
Izzatillo <izzatillo.mukhtorov@ydistri.com>
2022-10-31 01:36:29 +0500

    .

--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -2,12 +2,12 @@ import createHttpError from "http-errors";
 
 export const adminOnly = async (req, res, next) => {
   if (!(req.user.role === "admin"))
-    return next(createHttpError(403, "You are not authorized to do this"));
+    return next(createHttpError(401, "You are not authorized to do this"));
   next();
 };
 
 export const adminAndUserOnly = async (req, res, next) => {
   if (!(req.user.role === "admin" || req.user.role === "user"))
-    return next(createHttpError(403, "You are not authorized to do this"));
+    return next(createHttpError(401, "You are not authorized to do this"));
   next();
 };
